064e7ef13d218927a5ad698c9e78524070f08a7d
flolu <fllu@pm.me>
2020-03-13 13:14:40 +0100

    refactoring

---
+++ bake.py
@@ -0,0 +1,76 @@
+import bpy
+
+
+class BatchBake(bpy.types.Operator):
+    bl_idname = 'bb.bake'
+    bl_label = 'batch bake'
+    bl_options = {'UNDO'}
+
+    context = None
+    bake_material = None
+    bake_image = None
+
+    def execute(self, context):
+        self.context = context
+
+        self.prepare_bake()
+        self.check_setup()
+        self.bake_maps()
+        self.clean_up()
+
+        return {'FINISHED'}
+
+    def prepare_bake(self):
+        # TODO can i use self.context, too?
+        bpy.data.scenes[bpy.context.scene.name].render.engine = "CYCLES"
+
+        self.bake_material = bpy.data.materials.new(
+            name=self.context.scene.lowpoly_bake_obj + '_bake')
+
+        if bpy.context.active_object.data.materials:
+            bpy.data.objects[
+                self.context.scene.
+                lowpoly_bake_obj].data.materials[0] = self.bake_material
+        else:
+            bpy.data.objects[
+                self.context.scene.lowpoly_bake_obj].data.materials.append(
+                    self.bake_material)
+
+        self.bake_material.use_nodes = True
+        nodes = self.bake_material.node_tree.nodes
+        bake_node = nodes.new('ShaderNodeTexImage')
+        bake_node.select = True
+
+        self.bake_image = bpy.data.images.new(
+            self.context.scene.lowpoly_bake_obj + '_bake',
+            width=1024,
+            height=1024)
+        bake_node.image = self.bake_image
+
+        # TODO make visible in viewport and for rendering
+
+        bpy.data.objects[self.context.scene.highpoly_bake_obj].select_set(True)
+        bpy.data.objects[self.context.scene.lowpoly_bake_obj].select_set(True)
+
+    def check_setup(self):
+        print('check if everythin is alright')
+
+    def bake_maps(self):
+        if (self.context.scene.bake_diffuse):
+            self.bake_diffuse()
+
+    def clean_up(self):
+        print('clean up the mess!')
+
+    def bake_diffuse(self):
+        bpy.context.scene.cycles.samples = 1
+        bpy.context.scene.render.bake.use_pass_direct = False
+        bpy.context.scene.render.bake.use_pass_indirect = False
+        bpy.context.scene.render.bake.use_pass_color = True
+        bpy.ops.object.bake(type='DIFFUSE',
+                            use_clear=True,
+                            use_selected_to_active=True)
+
+        self.bake_image.filepath_raw = self.context.scene.bake_out_path + self.context.scene.lowpoly_bake_obj + '_diffuse.jpg'
+        self.bake_image.file_format = 'JPEG'
+        self.bake_image.save()
---
+++ panel.py
@@ -0,0 +1,39 @@
+import bpy
+
+
+class BatchBakerPanel(bpy.types.Panel):
+    bl_label = 'Batch Baking'
+    bl_idname = 'batch_baker'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Bake'
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        layout.prop_search(context.scene,
+                           'highpoly_bake_obj',
+                           context.scene,
+                           'objects',
+                           text='Highpoly')
+
+        row = layout.row()
+        layout.prop_search(context.scene,
+                           'lowpoly_bake_obj',
+                           context.scene,
+                           'objects',
+                           text='Lowpoly')
+
+        row = layout.row()
+        row.prop(context.scene, 'bake_diffuse', text='Diffuse')
+        row = layout.row()
+        row.prop(context.scene, 'bake_normal', text='Normal')
+        row = layout.row()
+        row.prop(context.scene, 'bake_ao', text='Ambient Occlusion')
+
+        col = layout.column()
+        col.prop(context.scene, 'bake_out_path', text='Output')
+
+        row = layout.row()
+        op = row.operator('bb.bake', text='Bake')
