a14f1a9063921b7ee11d5059d642c627a5f9da08
flolu <fllu@pm.me>
2020-03-13 11:54:44 +0100

    bake diffuse from high to low

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 bl_info = {
-    "name": "Batch Baker",
-    "author": "Florian Ludewig",
-    "description":
-    "Batch baking from low to high poly to multiple low poly meshes",
-    "blender": (2, 80, 0),
-    "version": (0, 0, 1),
-    "location": "View3D",
-    "category": "Generic"
+    'name': 'Batch Baker',
+    'author': 'Florian Ludewig',
+    'description':
+    'Batch baking from low to high poly to multiple low poly meshes',
+    'blender': (2, 80, 0),
+    'version': (0, 0, 1),
+    'location': 'View3D',
+    'category': 'Generic'
 }
 
 import bpy
+import os
 
 
 class BatchBakerPanel(bpy.types.Panel):
@@ -19,38 +20,87 @@ class BatchBakerPanel(bpy.types.Panel):
     bl_region_type = 'UI'
     bl_category = 'Bake'
 
-    def draw_header(self, _):
-        layout = self.layout
-        layout.label(text="", icon="COLORSET_12_VEC")
-
     def draw(self, context):
         layout = self.layout
 
         row = layout.row()
         layout.prop_search(context.scene,
-                           "bake_highpoly",
+                           'highpoly_bake_obj',
                            context.scene,
-                           "objects",
-                           text="Highpoly")
+                           'objects',
+                           text='Highpoly')
 
         row = layout.row()
         layout.prop_search(context.scene,
-                           "bake_lowpoly",
+                           'lowpoly_bake_obj',
                            context.scene,
-                           "objects",
-                           text="Lowpoly")
+                           'objects',
+                           text='Lowpoly')
 
         row = layout.row()
-        op = row.operator("bb.bake", text="Bake")
+        row.prop(context.scene, 'bake_diffuse', text='Diffuse')
+        row = layout.row()
+        row.prop(context.scene, 'bake_normal', text='Normal')
+        row = layout.row()
+        row.prop(context.scene, 'bake_ao', text='Ambient Occlusion')
+
+        col = layout.column()
+        col.prop(context.scene, 'bake_out_path', text='Output')
+
+        row = layout.row()
+        op = row.operator('bb.bake', text='Bake')
 
 
 class BatchBake(bpy.types.Operator):
-    bl_idname = "bb.bake"
-    bl_label = "batch bake"
-    bl_options = {"UNDO"}
+    bl_idname = 'bb.bake'
+    bl_label = 'batch bake'
+    bl_options = {'UNDO'}
 
     def execute(self, context):
-        print("goooO!!!")
+        print('start baking')
+
+        # TODO split into different methods which then serve as "comments"
+        bpy.data.scenes[bpy.context.scene.name].render.engine = "CYCLES"
+
+        bake_material = bpy.data.materials.new(
+            name=context.scene.lowpoly_bake_obj + '_bake')
+
+        if bpy.context.active_object.data.materials:
+            bpy.data.objects[context.scene.lowpoly_bake_obj].data.materials[
+                0] = bake_material
+        else:
+            bpy.data.objects[
+                context.scene.lowpoly_bake_obj].data.materials.append(
+                    bake_material)
+
+        bake_material.use_nodes = True
+        nodes = bake_material.node_tree.nodes
+        bake_node = nodes.new('ShaderNodeTexImage')
+        bake_node.select = True
+
+        bake_image = bpy.data.images.new(context.scene.lowpoly_bake_obj +
+                                         '_bake',
+                                         width=1024,
+                                         height=1024)
+        bake_node.image = bake_image
+
+        bpy.data.objects[context.scene.highpoly_bake_obj].select_set(True)
+        bpy.data.objects[context.scene.lowpoly_bake_obj].select_set(True)
+
+        bpy.context.scene.cycles.samples = 1
+        bpy.context.scene.render.bake.use_pass_direct = False
+        bpy.context.scene.render.bake.use_pass_indirect = False
+        bpy.context.scene.render.bake.use_pass_color = True
+        bpy.ops.object.bake(type='DIFFUSE',
+                            use_clear=True,
+                            use_selected_to_active=True)
+
+        bake_image.filepath_raw = context.scene.bake_out_path + context.scene.lowpoly_bake_obj + '_diffuse.jpg'
+        bake_image.file_format = 'JPEG'
+        bake_image.save()
+
+        # remove stuff used for baking
+
         return {'FINISHED'}
 
 
@@ -61,25 +111,48 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.Scene.bake_highpoly = bpy.props.StringProperty(
-        name="bake_highpoly",
-        default="",
-        description="Object to bake from (highpoly)",
+    bpy.types.Scene.highpoly_bake_obj = bpy.props.StringProperty(
+        name='highpoly_bake_obj',
+        description='The object you want to bake from')
+    # TODO multiple low poly objects (maybe use collection or multiselect)
+    bpy.types.Scene.lowpoly_bake_obj = bpy.props.StringProperty(
+        name='lowpoly_bake_obj',
+        description='The object you want to bake onto')
+
+    bpy.types.Scene.bake_diffuse = bpy.props.BoolProperty(
+        name='bake_diffuse',
+        description='Check if you want to bake a diffuse map',
+        default=True,
     )
-    bpy.types.Scene.bake_lowpoly = bpy.props.StringProperty(
-        name="bake_lowpoly",
-        default="",
-        description="Object to bake to (lowpoly)",
+    bpy.types.Scene.bake_normal = bpy.props.BoolProperty(
+        name='bake_normal',
+        description='Check if you want to bake a normal map',
+        default=True,
     )
+    bpy.types.Scene.bake_ao = bpy.props.BoolProperty(
+        name='bake_ao',
+        description='Check if you want to bake an ambient occlusion map',
+        default=True,
+    )
+
+    bpy.types.Scene.bake_out_path = bpy.props.StringProperty(
+        name="bake_out_path",
+        default='//',
+        description="The folder your maps will be saved to",
+        subtype='DIR_PATH')
 
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
 
-    del bpy.types.Scene.bake_highpoly
-    del bpy.types.Scene.bake_lowpoly
+    del bpy.types.Scene.highpoly_bake_obj
+    del bpy.types.Scene.lowpoly_bake_obj
+    del bpy.types.Scene.bake_diffuse
+    del bpy.types.Scene.bake_normal
+    del bpy.types.Scene.bake_ao
+    del bpy.types.Scene.bake_out_path
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     register()
