9536f84c14622e31a3cfe989369f734061ddb228
flolu <fllu@pm.me>
2020-03-20 13:57:58 +0100

    scene cleaning and code refactoring

--- bake.py
+++ bake.py
@@ -1,6 +1,5 @@
 import bpy
-
-from time import sleep
+import uuid
 
 
 class BatchBake(bpy.types.Operator):
@@ -8,44 +7,47 @@ class BatchBake(bpy.types.Operator):
     bl_label = 'batch bake'
     bl_options = {'UNDO'}
 
-    context = None
-    bake_material = None
-    bake_image = None
-    high = None
-
     def execute(self, context):
-        self.context = context
-        self.high = context.scene.highpoly_bake_obj
+        high = context.scene.highpoly_bake_obj
 
         for obj in bpy.data.objects:
             obj.hide_render = True
             obj.hide_viewport = False
-        bpy.data.objects[self.high].hide_render = False
 
-        if (self.context.scene.bake_multiple):
+        if (context.scene.bake_multiple):
             low_objects = bpy.data.collections[
                 context.scene.lowpoly_bake_obj].all_objects[:]
             for obj in low_objects:
-                self.bootstrap_bake(obj.name)
+                Bake(high, obj.name)
         else:
-            self.bootstrap_bake(self.context.scene.lowpoly_bake_obj)
+            Bake(high, context.scene.lowpoly_bake_obj)
 
         return {'FINISHED'}
 
-    def bootstrap_bake(self, low):
-        self.prepare_bake(low)
-        self.bake_maps(low)
-        self.clean_up(low)
 
-    def prepare_bake(self, low):
-        bpy.data.objects[low].hide_render = False
-        bpy.data.scenes[bpy.context.scene.name].render.engine = "CYCLES"
+class Bake():
+    def __init__(self, high, low):
+        self.high = high
+        self.low = low
 
-        self.bake_material = bpy.data.materials.new(name=low + '_bake')
-        if bpy.data.objects[low].data.materials:
-            bpy.data.objects[low].data.materials[0] = self.bake_material
+        self.prepare()
+        self.bake()
+        self.clean()
+
+    def prepare(self):
+        self.remember_original_settings()
+
+        bpy.data.scenes[bpy.context.scene.name].render.engine = 'CYCLES'
+        bpy.data.objects[self.low].hide_render = False
+        bpy.data.objects[self.high].hide_render = False
+
+        self.bake_material = bpy.data.materials.new(name=self.low +
+                                                    str(uuid.uuid4()))
+        if bpy.data.objects[self.low].data.materials:
+            bpy.data.objects[self.low].data.materials[0] = self.bake_material
         else:
-            bpy.data.objects[low].data.materials.append(self.bake_material)
+            bpy.data.objects[self.low].data.materials.append(
+                self.bake_material)
 
         self.bake_material.use_nodes = True
         nodes = self.bake_material.node_tree.nodes
@@ -53,25 +55,28 @@ class BatchBake(bpy.types.Operator):
         bake_node.select = True
 
         self.bake_image = bpy.data.images.new(
-            low + '_bake',
-            width=self.context.scene.output_size,
-            height=self.context.scene.output_size)
+            self.low + str(uuid.uuid4()),
+            width=bpy.context.scene.output_size,
+            height=bpy.context.scene.output_size)
         bake_node.image = self.bake_image
 
         bpy.data.objects[self.high].select_set(True)
-        bpy.context.view_layer.objects.active = bpy.data.objects[low]
+        bpy.context.view_layer.objects.active = bpy.data.objects[self.low]
 
-    def bake_maps(self, low):
-        if (self.context.scene.bake_diffuse):
-            self.bake_diffuse(low)
-        if (self.context.scene.bake_normal):
-            self.bake_normal(low)
-        if (self.context.scene.bake_ao):
-            self.bake_ao(low)
+    def bake(self):
+        if (bpy.context.scene.bake_diffuse):
+            self.bake_diffuse()
+        if (bpy.context.scene.bake_normal):
+            self.bake_normal()
+        if (bpy.context.scene.bake_ao):
+            self.bake_ao()
 
-    def clean_up(self, low):
-        print()
-        bpy.data.objects[low].hide_render = True
+    def clean(self):
+        bpy.data.objects[self.low].hide_render = True
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.images.remove(self.bake_image)
+        bpy.data.materials.remove(self.bake_material)
+        self.revert_original_settings()
 
     def setup_baking_settings(self):
         bpy.context.scene.cycles.samples = 1
@@ -79,33 +84,45 @@ class BatchBake(bpy.types.Operator):
         bpy.context.scene.render.bake.use_pass_indirect = False
         bpy.context.scene.render.bake.use_pass_color = True
 
-    def bake_diffuse(self, low):
+    def bake_diffuse(self):
         self.setup_baking_settings()
         bpy.ops.object.bake(type='DIFFUSE',
                             use_clear=True,
                             use_selected_to_active=True)
 
-        self.bake_image.filepath_raw = self.context.scene.bake_out_path + low + '_diffuse.jpg'
+        self.bake_image.filepath_raw = bpy.context.scene.bake_out_path + self.low + '_diffuse.jpg'
         self.bake_image.file_format = 'JPEG'
         self.bake_image.save()
 
-    def bake_normal(self, low):
+    def bake_normal(self):
         self.setup_baking_settings()
         bpy.ops.object.bake(type='NORMAL',
                             use_clear=True,
                             use_selected_to_active=True)
 
-        self.bake_image.filepath_raw = self.context.scene.bake_out_path + low + '_normal.jpg'
+        self.bake_image.filepath_raw = bpy.context.scene.bake_out_path + self.low + '_normal.jpg'
         self.bake_image.file_format = 'JPEG'
         self.bake_image.save()
 
-    def bake_ao(self, low):
-        bpy.context.scene.cycles.samples = self.context.scene.ao_samples
+    def bake_ao(self):
+        bpy.context.scene.cycles.samples = bpy.context.scene.ao_samples
         bpy.ops.object.bake(type='AO',
                             use_clear=True,
                             use_selected_to_active=True)
 
-        self.bake_image.filepath_raw = self.context.scene.bake_out_path + low + '_ao.jpg'
+        self.bake_image.filepath_raw = bpy.context.scene.bake_out_path + self.low + '_ao.jpg'
         self.bake_image.file_format = 'JPEG'
         # TODO save as bw image
         self.bake_image.save()
+
+    def remember_original_settings(self):
+        self.original_samples = bpy.context.scene.cycles.samples
+        self.original_render_engine = bpy.data.scenes[
+            bpy.context.scene.name].render.engine
+        self.original_material = bpy.data.objects[self.low].data.materials[0]
+
+    def revert_original_settings(self):
+        bpy.data.scenes[
+            bpy.context.scene.name].render.engine = self.original_render_engine
+        bpy.data.objects[self.low].data.materials[0] = self.original_material
+        bpy.context.scene.cycles.samples = self.original_samples
